44da94c07c04bfffb2a753ac7b01fca972ec38d7
piusgori <piusgori@gmail.com>
2022-09-15 23:50:56 +0300

    Added signup logic

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,5 +1,8 @@
 const User = require("../models/user");
 const HttpError = require("../models/http-error");
+const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 exports.getUsers = async (req, res, next) => {
   let foundUsers;
@@ -17,3 +20,53 @@ exports.getUsers = async (req, res, next) => {
   }
   res.status(200).json({ message: "Users found successfully", users: users });
 };
+
+exports.signUp = async (req, res, next) => {
+  const { name, email, password } = req.body;
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    const errorArray = errors.array();
+    return next(new HttpError('Unable to process', errorArray[0].msg, 422));
+  }
+  try {
+    const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+    const passwordResult = password.match(passwordPattern);
+    if(!passwordResult){
+        const message = 'Please enter a strong password of at least 8 characters containing a character, number and symbol';
+        return next(new HttpError('Unable to process password', message, 422));
+    }
+  } catch (err) {
+    return next(new HttpError('Unable to validate password input'));
+  }
+  try {
+    const foundUser = await User.findOne({ email });
+    if(foundUser){
+      return next(new HttpError('Unable to proceed', 'The user with the E-Mail address already exists', 422));
+    }
+  } catch (err) {
+    return next(new HttpError('Unable to check if user exists'));
+  }
+  let hashedPassword;
+  try {
+    hashedPassword = await bcrypt.hash(password, 12);
+  } catch (err) {
+    return next(new HttpError('Unable to hash the user password'));
+  }
+
+  const newUser = new User({email, name, password: hashedPassword});
+
+  try {
+    await newUser.save();
+  } catch(err) {
+    return next(new HttpError('Unable to save new user'));
+  }
+
+  let token;
+
+  try {
+    token - jwt.sign({ id: newUser._id, email: newUser.email, name: newUser.name }, 'topboy');
+  } catch (err) {
+    return next(new HttpError('Unable to generate user token'));
+  }
+  res.status(201).json({ message: 'Signed up successfully', id: newUser._id, name: newUser.name, email: newUser.email, token });
+}
--- routes/auth-route.js
+++ routes/auth-route.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const { body } = require('express-validator');
 const router = express.Router();
 
 const authController = require("../controllers/auth-controller");
 
 router.get("/", authController.getUsers);
+
+router.post('/signup', [
+    body('name').isLength({ min: 3 }).withMessage('Please enter a name of at least 3 characters long'),
+    body('email').normalizeEmail().isEmail().withMessage('Please enter a valid E-Mail address')
+], authController.signUp);
 
 module.exports = router;
